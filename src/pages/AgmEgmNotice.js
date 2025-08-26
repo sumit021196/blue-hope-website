b87e2e73c32ff0bf5d04e8ead8326ae11617d808
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-08-26 16:47:22 +0530

    AGM notice

--- src/pages/AgmEgmNotice.js
+++ src/pages/AgmEgmNotice.js
@@ -4,12 +4,13 @@ import '../App.css';
 import './AgmEgmNotice.css';
 
 function AgmEgmNotice() {
-  // Sample data for notice
+  // Notice data with PDF download link
   const notice = {
     id: 1,
     title: 'Notice of 1st Annual General Meeting of the Company',
-    content: 'This is to inform all the shareholders that the 1st Annual General Meeting of the Company will be held on Friday, the 15th day of March, 2024 at 11:00 A.M. (IST) at the Registered Office of the Company at [Company Address] to transact the following businesses...',
-    pdfUrl: '#'
+    content: 'This is to inform all the shareholders that the 1st Annual General Meeting of the Company will be held on Friday, the 15th day of March, 2024 at 11:00 A.M. (IST) at the Registered Office of the Company to transact the following businesses...',
+    pdfUrl: '/pdf/Notice of 1st AGM_Bluehope Solutions Limited.pdf',
+    fileName: 'Notice_of_1st_AGM_Bluehope_Solutions_Limited.pdf'
   };
 
   return (
@@ -22,8 +23,27 @@ function AgmEgmNotice() {
         <div className="agm-notice-list">
           <div className="notice-item">
             <h3 className="notice-title">
-              <Link to="#" className="notice-link">{notice.title}</Link>
+              <a 
+                href={notice.pdfUrl} 
+                className="notice-link" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                download={notice.fileName}
+              >
+                {notice.title}
+              </a>
             </h3>
+            <p className="notice-download">
+              <a 
+                href={notice.pdfUrl} 
+                className="download-link" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                download={notice.fileName}
+              >
+                Download Notice PDF
+              </a>
+            </p>
           </div>
         </div>
       </div>
