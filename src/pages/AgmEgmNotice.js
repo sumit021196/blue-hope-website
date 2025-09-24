e125c7bc7630566dbcf77d2fb5bd5c89aa4ec696
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2025-09-24 17:24:44 +0530

    Update AgmEgmNotice.js

--- src/pages/AgmEgmNotice.js
+++ src/pages/AgmEgmNotice.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import '../App.css';
 import './AgmEgmNotice.css';
 
 function AgmEgmNotice() {
-  // Notice data with PDF download link
-  const notice = {
-    id: 1,
-    title: 'Notice of 1st Annual General Meeting of the Company',
-    content: 'This is to inform all the shareholders that the 1st Annual General Meeting of the Company will be held on Friday, the 15th day of March, 2024 at 11:00 A.M. (IST) at the Registered Office of the Company to transact the following businesses...',
-    pdfUrl: `${window.location.origin}/pdf/Notice_of_1st_AGM_Bluehope_Solutions_Limited.pdf`,
-    fileName: 'Notice_of_1st_AGM_Bluehope_Solutions_Limited.pdf'
-  };
+  // Notices list (newest first)
+  const notices = [
+    {
+      id: 2,
+      title: 'Notice of adjourned AGM 2024-25 - Bluehope Solutions Limited',
+      fileName: 'Notice of adjourned AGM 2024-25_Bluehope Solutions Limited.pdf'
+    },
+    {
+      id: 1,
+      title: 'Notice of 1st Annual General Meeting of the Company',
+      fileName: 'Notice_of_1st_AGM_Bluehope_Solutions_Limited.pdf'
+    }
+  ];
 
   return (
     <div className="agm-page">
@@ -21,30 +25,35 @@ function AgmEgmNotice() {
       
       <div className="agm-content">
         <div className="agm-notice-list">
-          <div className="notice-item">
-            <h3 className="notice-title">
-              <a 
-                href={notice.pdfUrl} 
-                className="notice-link" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                download={notice.fileName}
-              >
-                {notice.title}
-              </a>
-            </h3>
-            <p className="notice-download">
-              <a 
-                href={notice.pdfUrl} 
-                className="download-link" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                download={notice.fileName}
-              >
-                Download Notice PDF
-              </a>
-            </p>
-          </div>
+          {notices.map(n => {
+            const pdfUrl = `${window.location.origin}/pdf/${encodeURIComponent(n.fileName)}`;
+            return (
+              <div className="notice-item" key={n.id}>
+                <h3 className="notice-title">
+                  <a 
+                    href={pdfUrl}
+                    className="notice-link" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    download={n.fileName}
+                  >
+                    {n.title}
+                  </a>
+                </h3>
+                <p className="notice-download">
+                  <a 
+                    href={pdfUrl}
+                    className="download-link" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    download={n.fileName}
+                  >
+                    Download Notice PDF
+                  </a>
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
